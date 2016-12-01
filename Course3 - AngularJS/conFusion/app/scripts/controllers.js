b9fe8e28eda49420242b1dc1e6afad13951eb826
Thomas <Thomas>
2016-12-01 17:04:09 +0100

    w4m2

--- Course3 - AngularJS/conFusion/app/scripts/controllers.js
+++ Course3 - AngularJS/conFusion/app/scripts/controllers.js
@@ -7,9 +7,21 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
+            $scope.showMenu = false;
+            $scope.message = "Loading ...";            
 
-            $scope.dishes= menuFactory.getDishes();
-
+            $scope.dishes= {};
+            
+            menuFactory.getDishes()
+            .then(
+                function(response) {
+                    $scope.dishes = response.data;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
                         
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -69,11 +81,19 @@ angular.module('confusionApp')
         }])
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
-            
-            $scope.dish = dish;
-            
+            $scope.dish = {};
+            $scope.showDish = false;
+            $scope.message="Loading ...";
+                        menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish=true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
@@ -98,7 +118,20 @@ angular.module('confusionApp')
 
             $scope.leader = corporateFactory.getLeader(0);
             $scope.promotion = menuFactory.getPromotion(0);
-            $scope.dish = menuFactory.getDish(0);
+            $scope.dish = {};
+            $scope.showDish = false;
+            $scope.message="Loading ...";
+
+            menuFactory.getDish(0)
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
         }])
 
         .controller('AboutController', ['$scope',  'corporateFactory', function($scope, corporateFactory) {
